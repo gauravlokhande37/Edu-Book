aa67ce4b4f70f43048005ff4e443f8f178143006
Gaurav Lokhande <83277251+gauravlokhande37@users.noreply.github.com>
2022-01-16 22:24:31 +0530

    Add files via upload

---
+++ switchstatement.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int age;
+    printf("Enter your age");
+    scanf("%d",&age);
+    switch(age)
+    case 12:
+    printf("the age is 12");
+    break;
+    case 23:
+    printf("the age is 23"):
+    break;
+    case 56:
+    printf("the age iz 56");
+    default:
+    printf()
+    }
